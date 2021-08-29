939128d04252caa2ddaa3697ae3e294340c60e91
Joan <guigomene15@gmail.com>
2021-08-29 00:54:26 -0300

    Exercício 03 feito.

--- Lista 03 - Estruturas de Dados Básicas/ex03.py
+++ Lista 03 - Estruturas de Dados Básicas/ex03.py
@@ -9,10 +9,16 @@ class Stack:
         self.items.append(item)
 
     def pop(self):
+        if self.items == []:
+            return
         return self.items.pop()
 
     def peek(self):
+        if self.items == []:
+            return
         return self.items[len(self.items) - 1]
 
     def size(self):
+        if self.items == []:
+            return 0
         return len(self.items)
