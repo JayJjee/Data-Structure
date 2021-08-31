1180a210e4a2ec71b9514ce04c5a4dd8ea0c5d56
Joan <guigomene15@gmail.com>
2021-08-31 19:56:06 -0300

    Primeira tentativa do Ex04

--- Lista 03 - Estruturas de Dados Básicas/ex04.py
+++ Lista 03 - Estruturas de Dados Básicas/ex04.py
@@ -16,9 +16,25 @@ class Queue:
         return len(self.fila)
 
 fila = Queue()
-tarPriori = input().split()
-atvRealizada = int(input())
-atvRestam = math.ceil(len(tarPriori)/2) - atvRealizada
+entrada1 = input().split()
+qntRealizada = int(input())
+atvRestam = math.ceil(len(entrada1)/2) - qntRealizada
+atividade = []
+prioridade = []
+
+for cont in range(len(entrada1)):
+    if cont % 2 != 0:
+        prioridade.append(entrada1[cont])
+    elif cont % 2 == 0:
+        atividade.append(entrada1[cont])
 
 print(f"Tamanho da fila: {atvRestam}")
+for cont2 in range(atvRestam):
+    print(f'Atividade: {atividade[cont2]}, Prioridade: #{prioridade[cont2]}')
 
+
+
+'''
+Comer 1 Academia 2 Dormir 3 ElaborarQuestoes 4 Banhar 5
+0
+'''
