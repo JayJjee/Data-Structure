0dad344fc8cb10e7047e8381cfc26760a6f9f315
Joan <guigomene15@gmail.com>
2021-08-31 23:02:59 -0300

    Consertando main

--- Lista 03 - Estruturas de Dados Básicas/ex04.py
+++ Lista 03 - Estruturas de Dados Básicas/ex04.py
@@ -15,22 +15,22 @@ class Queue:
     def size(self):
         return len(self.fila)
 
-fila = Queue()
 entrada1 = input().split()
 qntRealizada = int(input())
 atvRestam = math.ceil(len(entrada1)/2) - qntRealizada
-atividade = []
-prioridade = []
+atividade = Queue()
+prioridade = Queue()
 
 for cont in range(len(entrada1)):
     if cont % 2 != 0:
-        prioridade.append(entrada1[cont])
+        prioridade.enqueue(entrada1[cont])
     elif cont % 2 == 0:
-        atividade.append(entrada1[cont])
+        atividade.enqueue(entrada1[cont])
 
 print(f"Tamanho da fila: {atvRestam}")
 for cont2 in range(atvRestam):
     print(f'Atividade: {atividade[cont2]}, Prioridade: #{prioridade[cont2]}')
+
 
 
 '''
