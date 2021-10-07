e9ea05e09c8735ef56a262ce663f13de0124d2f4
Joan <guigomene15@gmail.com>
2021-10-07 18:21:00 -0300

    Começando conteudo de Arvores.

---
+++ Lista 06 - Árvores/ex02.py
@@ -0,0 +1,28 @@
+class ArvoreBinaria:
+    def BinaryTree(raiz):
+        return (raiz, (), ())
+
+    def insertLeft(raiz, )
+''' def __init__(self, root):
+        self.dado = root
+        self.esq = None
+        self.dir = None
+    
+    def insertEsq(self, newNode):
+        if self.esq == None:
+            self.esq = ArvoreBinaria(newNode)
+        else:
+            t = ArvoreBinaria(newNode)
+            t.esq = self.esq
+            self.esq = t
+    
+    def insertDir(self, newNode):
+        if self.dir == None:
+            self.dir = ArvoreBinaria(newNode)
+        else:
+            t = ArvoreBinaria(newNode)
+            t.dir = self.dir
+            self.dir = t'''
+
+myTree = ArvoreBinaria()
+print(myTree)
